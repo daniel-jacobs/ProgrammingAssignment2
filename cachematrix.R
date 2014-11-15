bc5ed2a3df5883a50164bde320ec2f4ca69e1e31
daniel-jacobs <DanielJacobs@daniel-jacobss-macbook-pro.local>
2014-11-15 09:45:44 -0600

    Programming Assignment 2
    Functions that eliminate excessive computation when determining the
    inverse of a matrix by cacheing its value for future use.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions eliminate excessive computation when determining the
+## inverse of a matrix by cacheing its value for future use.
 
-## Write a short comment describing this function
+## This function creates a special "Matrix" object that can potentially
+## store and retrieve information on the matrix and its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(new_matrix){
+        x <<- new_matrix
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix and caches it for 
+## further use.
 
 cacheSolve <- function(x, ...) {
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinv(inv)
+    inv
         ## Return a matrix that is the inverse of 'x'
 }
